00f0f28e148ad0b84c5db70d6c0339d6280fc55b
kserrano77 <forzzaserrano@gmail.com>
2025-08-09 20:20:09 -0600

    Update MonthlyBlock.jsx

--- src/components/payments/MonthlyBlock.jsx
+++ src/components/payments/MonthlyBlock.jsx
@@ -131,7 +131,7 @@ const MonthlyBlock = ({
       
       console.log('📤 Enviando datos a Edge Function:', requestData);
 
-      const response = await supabase.functions.invoke('send-payment-receipt', {
+      const response = await supabase.functions.invoke('send-payment-receipt-v2', {
         body: requestData
       });
 
